05dc75d6691fa2d5fa205be76d685930babb722f
Carlos Lamosa <carlos.lamosa@gmail.com>
2020-09-17 20:14:35 +0200

    Primer commit

---
+++ docs/precache-manifest.3d7ee469d93dcd0f1ac9d184d13410c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17014ef118d1bcfdd2916486aa676503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb0a17a7a5675479714c",
+    "url": "/static/css/main.ef299d45.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0a17a7a5675479714c",
+    "url": "/static/js/main.376354d1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
